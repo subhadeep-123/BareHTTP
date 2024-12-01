d0ad08eb00b4416b6713fb0e245f51b6b31260db
Subhadeep Banerjee <subhadeep762@gmail.com>
2024-12-02 00:56:45 +0530

    Refactor main.rs

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,7 +1,6 @@
 use std::fs;
-use std::io::prelude::*;
-use std::net::TcpListener;
-use std::net::TcpStream;
+use std::io::{prelude::*, BufReader};
+use std::net::{TcpListener, TcpStream};
 use std::thread;
 use std::time::Duration;
 
@@ -21,17 +20,13 @@ fn main() {
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    let mut buffer = [0; 1024];
+    let buf_reader = BufReader::new(&stream);
 
-    stream.read(&mut buffer).unwrap();
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let get = b"GET / HTTP/1.1\r\n";
-
-    let sleep = b"GET /sleep HTTP/1.1\r\n";
-
-    let (status_line, filename) = if buffer.starts_with(get) {
+    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
         ("HTTP/1.1 200 OK", "index.html")
-    } else if buffer.starts_with(sleep) {
+    } else if request_line == "GET /sleep HTTP/1.1" {
         thread::sleep(Duration::from_secs(5));
         ("HTTP/1.1 200 OK", "index.html")
     } else {
@@ -39,14 +34,9 @@ fn handle_connection(mut stream: TcpStream) {
     };
 
     let contents = fs::read_to_string(format!("./templates/{}", filename)).unwrap();
+    let length = contents.len();
 
-    let response = format!(
-        "{}\r\nContent-Length: {}\r\n\r\n{}",
-        status_line,
-        contents.len(),
-        contents
-    );
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+    stream.write_all(response.as_bytes()).unwrap();
 }
